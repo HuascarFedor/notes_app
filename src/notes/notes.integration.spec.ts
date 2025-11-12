dd6dcbd6cd0aa31a19944b56e99bf5780cc8b593
HuascarFedor <huascar.fedor@gmail.com>
2025-11-12 09:53:59 -0400

    prueba de integracion agregada

--- src/notes/notes.integration.spec.ts
+++ src/notes/notes.integration.spec.ts
@@ -30,7 +30,7 @@ describe('Notas Integration Tets', () => {
 
   // Despues de cada prueba
   afterEach(async () => {
-    // await repository.query('DELETE FROM note;');
+    await repository.query('DELETE FROM note;');
   });
 
   // Despues de finalizado las pruebas
@@ -62,5 +62,73 @@ describe('Notas Integration Tets', () => {
       expect(notasEnBD.title).toBe(nuevaNota.title);
       expect(notasEnBD.content).toBe(nuevaNota.content);
     }
+  });
+
+  it('Deberia obtener todas las notas', async () => {
+    // Crear varias notas
+    const notes = [
+      {
+        title: 'Nota 1',
+        content: 'Contenido de prueba 1',
+      },
+      {
+        title: 'Nota 2',
+        content: 'Contenido de prueba 2',
+      },
+      {
+        title: 'Nota 3',
+        content: 'Contenido de prueba 3',
+      },
+    ];
+
+    // Gardar las notas en la base de datos
+    await repository.save(notes);
+
+    // Obtener las notas mediante el servicio
+    const result = await service.findAll();
+
+    // Verificar que se obtengan las notas
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.length).toBe(3);
+
+    // Verificar los datos
+    const titles = result.map((n) => n.title);
+    expect(titles).toContain('Nota 1');
+    expect(titles).toContain('Nota 2');
+    expect(titles).toContain('Nota 3');
+
+    // Verificar la integridad del contenido
+    expect(result[0]).toHaveProperty('id');
+    expect(result[0]).toHaveProperty('title');
+    expect(result[0]).toHaveProperty('content');
+  });
+
+  it('Deberia obtener una nota por ID', async () => {
+    // Crear una nota en la base de datos
+    const note = {
+      title: 'Nota de prueba',
+      content: 'Contenido de prueba',
+    };
+    const noteSave = await repository.save(note);
+
+    // Obtener una nota mediante el servicio
+    const result = await service.findOne(noteSave.id);
+
+    // Verificar que la nota sea la misma
+    expect(result).toBeDefined();
+    expect(result.title).toBe(note.title);
+    expect(result.content).toBe(note.content);
+
+    // Verificar la integridad del contenido
+    expect(result).toHaveProperty('id');
+    expect(result).toHaveProperty('title');
+    expect(result).toHaveProperty('content');
+  });
+
+  it('findOne - Deberia lanzar NotFoundException si la nota no existe', async () => {
+    // Verificar que la nota sea la misma
+    await expect(service.findOne(9999)).rejects.toThrow(
+      `Nota con ID 9999 no existe`,
+    );
   });
 });
